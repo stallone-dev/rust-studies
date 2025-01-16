abe6cf7b034a2bd6269320beed7197861c23ebe3
stallone-dev <stallone.developer@gmail.com>
2025-01-16 18:49:42 -0300

    borrowing studies

---
+++ fundamentos/src/_7_borrowing.rs
@@ -0,0 +1,6 @@
+/*
+    -- Borrowing --
+
+    Um dos problemas mais antigos e catastróficos da programação é a gestão de memória: Para executar algo, precisa-se guardar um pouco de memória em algum lugar, alguma variável, e manipulá-la pode gerar erros e falhas de segurança graves.
+
+*/
